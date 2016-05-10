717db9f0f566a1cd73480fccce5b3468f06d907d
maryse-caron60400 <maryse.caron3@wanadoo.fr>
2016-05-10 10:35:14 +0200

    corr exo4

--- Prairie PHP4/exo4.php
+++ Prairie PHP4/exo4.php
@@ -1,7 +1,11 @@
 <?php
 for ($i = 1;$i<101;$i++){
-  echo $i;
+  if ($i%3==0 && $i%5==0) {
+    echo "Fizz Buzz";# code...
+  }elseif($i%3==0){
+  echo "Fizz";} elseif ($i%5==0) {
+    echo "Buzz";
+  }else echo $i;
   echo " ";
-  if(!($i%3)){
-  echo "Fizz";
-} ?>
+  }
+ ?>
